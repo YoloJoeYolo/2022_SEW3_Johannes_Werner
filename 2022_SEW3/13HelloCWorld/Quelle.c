bee756338eea80053997b1594969fe78fd64446f
YoloJoeYolo <johannes.werner@josephinum.at>
2021-11-24 15:05:33 +0100

    Beginn mit C

---
+++ 2022_SEW3/13HelloCWorld/Quelle.c
@@ -0,0 +1,7 @@
+#include<stdio.h>	// Abkürzung für "standard Input Output"
+
+int main() {
+	printf("Hello C World");
+	return 0;	// wenn alles gut gegangen ist, dann 0 als Rückgabewert
+				// bei einem Fehler irgend ein anderer Wert --> meist ein Fehlercode
+}
